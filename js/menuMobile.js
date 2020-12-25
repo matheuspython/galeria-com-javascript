e572510fab5d1aaba255738f3f5942600300acba
matheus souza <ruivorobado@gmail.com>
2020-12-24 23:57:59 -0300

    adicionando o menu mobile

---
+++ js/menuMobile.js
@@ -0,0 +1,17 @@
+var menu = document.querySelector('aside.aside')
+var cobreTela = document.querySelector('.cobre-tela')
+
+
+
+function abreMenu() {
+    menu.style.display = 'block'
+    cobreTela.style.display = 'block'
+
+}
+
+function fechaMenu() {
+    menu.style.display = 'none'
+    cobreTela.style.display = 'none'
+
+
+}
